b12da4b78f032196845c9795c4e4ffe6c5900014
AndeeeeeZ <zhuandeee@gmail.com>
2026-08-01 17:53:03 -0400

    Reorganized into folders

--- Other/SceneLoader.cs
+++ Other/SceneLoader.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*
+* Helper class to load scene
+*/
+
 public class SceneLoader : MonoBehaviour
 {
+    public string targetSceneName; 
+
+    public void LoadTargetScene()
+    {
+        LoadScene(targetSceneName);
+    }
+
     public void LoadScene(string name)
     {
         SceneManager.LoadScene(name, LoadSceneMode.Single); 
