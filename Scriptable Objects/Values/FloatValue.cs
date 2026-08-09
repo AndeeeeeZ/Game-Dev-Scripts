a337b2773573eadafe3dd2e6161434eb9c1e4a5e
AndeeeeeZ <zhuandeee@gmail.com>
2026-08-09 14:54:12 -0400

    Update FloatValue.cs
    Added a custom editor script that shows allows the user to change the value when another object is serializing and referencing to the FloatValue object

--- Scriptable Objects/Values/FloatValue.cs
+++ Scriptable Objects/Values/FloatValue.cs
@@ -1,13 +1,126 @@
-using UnityEngine; 
+using UnityEngine;
 
+[CreateAssetMenu(menuName = "Values/FloatValue")]
 public class FloatValue : ScriptableObject
 {
-    [SerializeField] private float value; 
+    [SerializeField] private float value;
 
-    public float Value => value; 
+    public float Value => value;
 
     public void SetValue(float v)
     {
-        value = v; 
+        value = v;
     }
 }
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(FloatValue))]
+public class FloatValueDrawer : PropertyDrawer
+{
+    private const float IndentWidth = 15f;
+    private const float Spacing = 2f;
+
+    public override void OnGUI(
+        Rect position,
+        SerializedProperty property,
+        GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Draw the foldout arrow and label
+        Rect foldoutRect = new Rect(
+            position.x,
+            position.y,
+            EditorGUIUtility.labelWidth,
+            EditorGUIUtility.singleLineHeight
+        );
+
+        property.isExpanded = EditorGUI.Foldout(
+            foldoutRect,
+            property.isExpanded,
+            label,
+            true
+        );
+
+        // Draw the ScriptableObject reference
+        Rect objectRect = new Rect(
+            position.x + EditorGUIUtility.labelWidth,
+            position.y,
+            position.width - EditorGUIUtility.labelWidth,
+            EditorGUIUtility.singleLineHeight
+        );
+
+        EditorGUI.PropertyField(
+            objectRect,
+            property,
+            GUIContent.none
+        );
+
+        // Draw the value if expanded and a FloatValue is assigned
+        if (property.isExpanded && property.objectReferenceValue != null)
+        {
+            SerializedObject floatValueObject =
+                new SerializedObject(property.objectReferenceValue);
+
+            floatValueObject.Update();
+
+            SerializedProperty value =
+                floatValueObject.FindProperty("value");
+
+            float childY =
+                position.y +
+                EditorGUIUtility.singleLineHeight +
+                Spacing;
+
+            // Indented label for the child value
+            Rect valueLabelRect = new Rect(
+                position.x + IndentWidth,
+                childY,
+                EditorGUIUtility.labelWidth - IndentWidth,
+                EditorGUIUtility.singleLineHeight
+            );
+
+            EditorGUI.LabelField(
+                valueLabelRect,
+                "Value"
+            );
+
+            // Keep the input field aligned with the reference field
+            Rect valueFieldRect = new Rect(
+                position.x + EditorGUIUtility.labelWidth,
+                childY,
+                position.width - EditorGUIUtility.labelWidth,
+                EditorGUIUtility.singleLineHeight
+            );
+
+            EditorGUI.PropertyField(
+                valueFieldRect,
+                value,
+                GUIContent.none
+            );
+
+            floatValueObject.ApplyModifiedProperties();
+        }
+
+        EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(
+        SerializedProperty property,
+        GUIContent label)
+    {
+        float height = EditorGUIUtility.singleLineHeight;
+
+        if (property.isExpanded && property.objectReferenceValue != null)
+        {
+            height += EditorGUIUtility.singleLineHeight + Spacing;
+        }
+
+        return height;
+    }
+}
+
+#endif
